dfeb3fb5fcce39b4c5eb7919d7945de46e2ce7f3
Subhajit Mondal <subhajit25mondal@gmail.com>
2018-07-16 01:55:17 +0530

    performance improvements

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,13 +10,14 @@ import { AsyncPipe } from '@angular/common';
 })
 export class HomePage {
 
+  url = 'http://api.openweathermap.org/data/2.5/weather?lat=23.5204&lon=87.3119&APPID=63a61c83d2406647c0ed9b36cad89d1f';
   weather: Observable<any>;
   city: any;
   country : any;
 
   constructor(public navCtrl: NavController, public httpClient: HttpClient) {
 
-    this.weather = this.httpClient.get('http://api.openweathermap.org/data/2.5/weather?lat=23.5204&lon=87.3119&APPID=63a61c83d2406647c0ed9b36cad89d1f');
+    this.weather = this.httpClient.get(this.url);
     this.weather
     .subscribe(data => {
       this.city=data.name;
