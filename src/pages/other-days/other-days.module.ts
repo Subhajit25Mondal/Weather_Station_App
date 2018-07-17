2053cb5b23c6403b9cf3424962a9ac3a7c7d40da
Subhajit Mondal <subhajit25mondal@gmail.com>
2018-07-18 00:18:09 +0530

    layout gen

---
+++ src/pages/other-days/other-days.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { OtherDaysPage } from './other-days';
+
+@NgModule({
+  declarations: [
+    OtherDaysPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(OtherDaysPage),
+  ],
+})
+export class OtherDaysPageModule {}
